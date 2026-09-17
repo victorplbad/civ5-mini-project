4f4e4b8a07ad2606fd766c121976565c3fca0f57
victorplbad <84019887+victorplbad@users.noreply.github.com>
2026-09-17 12:30:18 +0200

    part 2 done
    proper grid coloring

--- Assets/scripts/create hex grid/HexMetrics.cs
+++ Assets/scripts/create hex grid/HexMetrics.cs
@@ -44,7 +44,7 @@ public static class HexMetrics
     public static Vector3 GetBridge(HexDirection direction)
     {
         return (corners[(int)direction] + corners[(int)direction + 1]) *
-            0.5f * blendFactor;
+            blendFactor;
     }
 
 }
