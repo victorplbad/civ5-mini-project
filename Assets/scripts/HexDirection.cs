2eb66a0e8aee82f230d8cb19c54c131d8c0f36a0
victorplbad <84019887+victorplbad@users.noreply.github.com>
2026-09-17 09:55:32 +0200

    color mesh mixing

--- Assets/scripts/HexDirection.cs
+++ Assets/scripts/HexDirection.cs
@@ -1,8 +1,13 @@
-
-
-
-
 public enum HexDirection
 {
     NE, E, SE, SW, W, NW
+}
+
+public static class HexDirectionExtensions
+{
+
+    public static HexDirection Opposite(this HexDirection direction)
+    {
+        return (int)direction < 3 ? (direction + 3) : (direction - 3);
+    }
 }
--- Assets/scripts/create hex grid/HexGrid.cs
+++ Assets/scripts/create hex grid/HexGrid.cs
@@ -74,6 +74,30 @@ public class HexGrid : MonoBehaviour
         
 		cell.color = defaultColor;
 
+        if (x > 0)
+        {
+            cell.SetNeighbor(HexDirection.W, cells[i - 1]);
+        }
+        if (z > 0)
+        {
+            if ((z & 1) == 0)
+            {
+                cell.SetNeighbor(HexDirection.SE, cells[i - width]);
+                if (x > 0)
+                {
+                    cell.SetNeighbor(HexDirection.SW, cells[i - width - 1]);
+                }
+            }
+            else
+            {
+                cell.SetNeighbor(HexDirection.SW, cells[i - width]);
+                if (x < width - 1)
+                {
+                    cell.SetNeighbor(HexDirection.SE, cells[i - width + 1]);
+                }
+            }
+        }
+
 
         Text label = Instantiate<Text>(cellLabelPrefab);
 		label.rectTransform.SetParent(gridCanvas.transform, false);
