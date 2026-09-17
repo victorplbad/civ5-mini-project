2b99b9e1573ac09a8260b2876fbb877f088da0c8
victorplbad <84019887+victorplbad@users.noreply.github.com>
2026-09-17 09:16:29 +0200

    bug fix
    input system got delete fixed

---
+++ Assets/scripts/HexDirection.cs
@@ -0,0 +1,8 @@
+
+
+
+
+public enum HexDirection
+{
+    NE, E, SE, SW, W, NW
+}
--- Assets/scripts/create hex grid/HexCell.cs
+++ Assets/scripts/create hex grid/HexCell.cs
@@ -6,5 +6,8 @@ public class HexCell : MonoBehaviour
 
     public Color color;
 
+    [SerializeField]
+    HexCell[] neighbors;
+
 
 }
